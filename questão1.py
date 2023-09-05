2eb0bba351d06b4fffff642e21f992ffd30a0272
DudaribeiroA06 <142848863+DudaribeiroA06@users.noreply.github.com>
2023-09-04 21:56:07 -0300

    Add files via upload

---
+++ questão1.py
@@ -0,0 +1,7 @@
+sal=float(input("Digite o valor do salario: "))
+finan=float(input("Digite o valor do financiamento: "))
+if (finan<=5*sal):
+    print("financiamento concedido")
+else:
+    print("financiamento negado")
+print("Obrigado por nos consultar")
